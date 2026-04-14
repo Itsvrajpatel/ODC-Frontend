f100b9fd00c364e4604c5c46462fb20ada4836b2
VRAJ K PATEL <99316553+Itsvrajpatel@users.noreply.github.com>
2026-04-14 19:18:15 +0530

    added banner component to home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Features from '../components/Features';
+import Banner from '../components/Banner';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <About />
       <Features />
+      <Banner />
     </main>
   );
 }
