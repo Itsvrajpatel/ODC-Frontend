883ddf033c423767c4198ba19d758139388f3387
VRAJ K PATEL <99316553+Itsvrajpatel@users.noreply.github.com>
2026-04-14 22:22:52 +0530

    added contact form to home page

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import Hero from '../components/Hero';
 import About from '../components/About';
 import Features from '../components/Features';
 import Banner from '../components/Banner';
+import ContactForm from '../components/ContactForm';
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <About />
       <Features />
       <Banner />
+      <ContactForm />
     </main>
   );
 }
